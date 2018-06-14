09b53bec2c3a6f8dd13021eb126e177107cff64f
seru71 <pawel.sztromwasser@gmail.com>
2018-06-14 14:52:33 +0200

    added scrfipt for downloading ENCODE DNAseq data

--- make_data_tree.sh
+++ make_data_tree.sh
@@ -7,6 +7,9 @@ TISSUES=${DATA_ROOT}/tissues
 TISSUES_CELLTYPE=${DATA_ROOT}/tissues/celltype
 TISSUES_ORGAN=${DATA_ROOT}/tissues/organ
 TSS=${DATA_ROOT}/transcription_start_sites
+CHROMATIN=${DATA_ROOT}/chromatin
+CHROMATIN_RAW=${DATA_ROOT}/chromatin/raw
+
 
 #Make dirs
 printf "Making directories tree under %s\n" ${DATA_ROOT}
@@ -14,6 +17,8 @@ mkdir -p ${GENES_DB_SOURCES} -v
 mkdir -p ${TISSUES_CELLTYPE} -v
 mkdir -p ${TISSUES_ORGAN} -v
 mkdir -p ${TSS} -v
+mkdir -p ${CHROMATIN} -v
+mkdir -p ${CHROMATIN_RAW} -v
 
 
 # Download genes
@@ -38,3 +43,20 @@ tar -xzf ${TISSUES}/facet_expressed_enhancers.tgz -C ${TISSUES_ORGAN} --wildcard
 # Download transcription start sites
 printf "Acquiring transcription start sites fantom5 data\n"
 wget -O ${TSS}/promoter_data.bed 'http://promoter.binf.ku.dk/viewer.php?match=and&sort-by=donotsort&end-site=249250621&start-site=1&chr-number=ALL&toggle=basic&return=download'
+
+#
+# Download ENCODE accessible chromatin data
+PREDEFINED_DNASEQ_SOURCES=predefined_dnaseq_data_sources
+printf "Acquiring ENCODE accessible chromatin data\n"
+# download raw BED files
+awk -F '\t' '$43 ~ hg19 {print $42}' ${PREDEFINED_DNASEQ_SOURCES}/ENCODE_DNase_seq.metadata.tsv | wget -i - -P ${CHROMATIN_RAW}
+# generate collapsing script & run it
+python ${PREDEFINED_DNASEQ_SOURCES}/collapse_tissue_beds.py ${PREDEFINED_DNASEQ_SOURCES}/ENCODE_DNase_seq.metadata.tsv hg19 > ${CHROMATIN}/collapse_hg19.sh
+chmod u+x ${CHROMATIN}/collapse_hg19.sh && ${CHROMATIN}/collapse_hg19.sh
+# delete raw BEDs to save space
+#rm -r ${CHROMATIN_RAW}
+
+
+
+
+
