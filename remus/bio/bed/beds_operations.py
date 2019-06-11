3a0a894ac4936db325508101d4801af706d259d8
seru71 <pawel.sztromwasser@gmail.com>
2019-06-11 08:23:23 +0200

    optional merge is optional now

--- remus/bio/bed/beds_operations.py
+++ remus/bio/bed/beds_operations.py
@@ -45,7 +45,7 @@ class BedOperations:
         if len(beds) == 0: 
             raise MissingBedsException('Empty BED list for intersect operation')
                         
-        accumulation = beds[0]
+        accumulation = beds[0].merge() if merge else beds[0]
         for bed in beds[1:]:
             accumulation = accumulation.intersect(bed, **kwargs)
             
